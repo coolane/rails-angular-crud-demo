c8e6ebc1a85608f6fd6202eea555a6589611b7ca
roblane09 <roblane09@gmail.com>
2014-07-18 16:01:23 -0700

    routes for api and item_list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'pages/item_list'
+
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :items, only: [:show]
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
