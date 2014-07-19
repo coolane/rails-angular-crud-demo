ecd49d1eed08199b82627224293f8f37af6a6970
roblane09 <roblane09@gmail.com>
2014-07-18 17:21:52 -0700

    new database seed to view and test our functionality.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+items = Item.create([
+            { label: 'Test Item', price: 8.99, color: 'blue'}])
